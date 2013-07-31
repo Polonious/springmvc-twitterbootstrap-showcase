71039706ff71328e8f30067cb665c7d5478a8a7b
TedLiang <tedliang.email@gmail.com>
2013-07-31 21:42:49 +1000

    implement file download

--- src/main/java/org/springframework/samples/mvc/wikitext/WikitextController.java
+++ src/main/java/org/springframework/samples/mvc/wikitext/WikitextController.java
@@ -2,6 +2,7 @@ package org.springframework.samples.mvc.wikitext;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
@@ -9,54 +10,61 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.ProjectHelper;
-import org.springframework.mvc.extensions.ajax.AjaxUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/wikitext")
 public class WikitextController {
 
-	@ModelAttribute
-	public void ajaxAttribute(WebRequest request, Model model) {
-		model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(request));
-	}
-
-	@RequestMapping(method=RequestMethod.GET)
-	public void fileUploadForm() {
+	private static String wikiHome = "/home/polonious/wikitext";
+	
+	@RequestMapping(method=RequestMethod.GET, value = "/{id}/{filename:.+}")
+	public void download(@PathVariable("id") String id, 
+			@PathVariable("filename") String filename, 
+			HttpServletResponse response) throws FileNotFoundException, IOException {
+		File pdfFile = new File(wikiHome+"/tmp/wikitext"+id+".pdf");
+		pdfFile.deleteOnExit();
+		
+		if(pdfFile.exists()){
+			response.setContentType("application/pdf");
+	        response.setContentLength(Long.valueOf(pdfFile.length()).intValue());
+	        response.setHeader("Content-Disposition","attachment; filename=\""+filename+"\"");
+	        FileCopyUtils.copy(new FileInputStream(pdfFile), 
+                        response.getOutputStream());
+        }
 	}
 
 	@RequestMapping(method=RequestMethod.POST)
-	public void processUpload(@RequestParam MultipartFile file, HttpServletResponse response) throws IOException {
-		File wikitextFile = File.createTempFile("wikitext", ".textile", new File("/home/polonious/wikitext"));
+	@ResponseBody
+	public String processUpload(@RequestParam("file") MultipartFile file,
+			@RequestParam(value = "title", defaultValue = "Documentation") String title,
+			HttpServletResponse response) throws IOException {
+		File wikitextFile = File.createTempFile("wikitext", ".textile", 
+				new File(wikiHome+"/tmp"));
+		wikitextFile.deleteOnExit();
+		file.transferTo(wikitextFile);
+
 		String name = FilenameUtils.getBaseName(wikitextFile.getAbsolutePath());
 		
-		file.transferTo(wikitextFile);
-		
-		File buildFile = new File("/home/polonious/wikitext/build.xml");
-		Project p = new Project();
-		p.setUserProperty("ant.file", buildFile.getAbsolutePath());
-		p.setProperty("document.name", name);
-		p.init();
+		File buildFile = new File(wikiHome+"/build.xml");
+		Project project = new Project();
+		project.setUserProperty("ant.file", buildFile.getAbsolutePath());
+		project.setProperty("document.name", name);
+		project.setProperty("bookTitle", title);
+		project.init();
 		ProjectHelper helper = ProjectHelper.getProjectHelper();
-		p.addReference("ant.projectHelper", helper);
-		helper.parse(p, buildFile);
-		p.executeTarget(p.getDefaultTarget());
+		project.addReference("ant.projectHelper", helper);
+		helper.parse(project, buildFile);
+		project.executeTarget(project.getDefaultTarget());
 		
-		File pdfFile = new File("/home/polonious/wikitext/"+name+".pdf");
-		
-		response.setContentType("application/pdf");
-        response.setContentLength(Long.valueOf(pdfFile.length()).intValue());
-        response.setHeader("Content-Disposition","attachment; filename=\"Documentation.pdf\"");
-        FileCopyUtils.copy(new FileInputStream(pdfFile), 
-                        response.getOutputStream());
+		return name.substring(8);
 	}
 	
 }
